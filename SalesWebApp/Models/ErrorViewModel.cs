faa8fb3b1d4f0b475d82bb0964979d6719830638
Joao Carlos <buenotjoao07@gmail.com>
2024-05-08 15:27:46 -0300

    Iniciando um Projeto Web ASP.NET Core

---
+++ SalesWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SalesWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
